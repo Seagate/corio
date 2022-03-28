78c7cee799c82d94f1b098b93040c85963372fb3
Vishal Dhobale <vishal.dhobale@seagate.com>
2022-03-28 17:31:44 +0530

    exception module for corio.
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

---
+++ src/commons/exception.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+#
+
+"""Exception module for corio tool."""
+
+
+class HealthCheckError(Exception):
+    """class for health check error."""
+
+    def __init__(self, message=''):
+        """Initialize health check error."""
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        """print error message."""
+        return self.message
