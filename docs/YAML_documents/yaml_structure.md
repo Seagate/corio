53d2048e64f372627b037895ea8534159138199f
Vishal Dhobale <69301843+DhobaleVishal@users.noreply.github.com>
2022-04-07 14:09:15 +0530

    Cortx 30403 readme update and rvf (#35)
    * fixed parameter in workload.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * fix for 'Invalid cross-device link' error.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * ReadMe update/correction for new support.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>
    
    * Fix if master details are missing.
    
    Signed-off-by: Vishal Dhobale <vishal.dhobale@seagate.com>

--- docs/YAML_documents/yaml_structure.md
+++ docs/YAML_documents/yaml_structure.md
@@ -11,44 +11,47 @@ A test yaml file should have the following parameters based on workloads to exec
 * sessions_per_node
 * sessions
 * tool
-
+* operation
 ---
 
-TEST_ID can be any unique string which can be treated as filtering through execution logs.  
+**TEST_ID** can be test ticket which is used for JIRA update or any unique string which is used to 
+create execution log file and for searching through execution logs.  
 
-object_size can be given as fixed values, list of values and range of values for test 
-workload, so those can have the following keys in it.
+**object_size** can be given as fixed values, list of values and range of values for test workload, 
+Range for object size can be mentioned using start and end.
 * start
 * end
 
-part_size can be given as fixed values and range of values for multipart workloads, so  those 
+**part_size** can be given as fixed values and range of values for multipart workloads, so those 
 can have the following keys in it.
 * start
-* end 
+* end
 
-sessions_per_node multiplied with number_of_nodes (command line argument '-nn') will be treated 
+Start and End range parameters are **object_size**/**part_size** for which test will be executed.
+Sizes can be given from bytes, KB (1000 Bytes), MB (1000 KB) up to TB (1000 GB). We can also use 
+KiB (1024 Bytes) format as well.
+
+**sessions_per_node** multiplied with number_of_nodes (command line argument '-nn') will be treated 
 as total number of sessions for a test workload. Default value of number_of_nodes is 1.
 
-sessions specified in YAML file will be directly used to create total sessions. 
+**sessions** specified in YAML file will be directly used to create total sessions. 
 
-range_read key is used to perform range read operations in a workload. 
+**range_read** key is used to perform range read operations in a workload. 
 
-part_range is range of numbers from which number of parts for multipart workloads can be 
-calculated. 
+**part_range** is range of numbers from which random number of parts for multipart workloads can be 
+calculated.
 
-Start and End range parameters are object/part sizes for which test will be executed.
-Sizes can be given from bytes, KB (1000 Bytes), MB (1000 KB) up to TB (1000 GB). We can also use KiB (1024 Bytes) format as well.
+**min_runtime** can be specified from seconds up to days. For example: 1d1h, 1h or 2d1h2s.
 
-min_runtime can be specified from seconds up to days. For example: 1d1h, 1h or 2d1h2s.
+**tool** can be specified from one of these **s3api**, **s3bench** or **warp**. 
+    note:- s3bench and warp support to be added.
 
-tool can be specified from one of these **s3bench**, **s3api** or **warp**.
-
-operation should be specified according to type of workload and need to map it in corio.py with 
+**operation** should be specified according to type of workload and need to map it in corio.py with 
 appropriate test script.
 
 ---
 
-Sample YAML file can be found at **./sample_file.yaml**
+Sample YAML file can be found at [sample file](sample_file.yaml)
 
-YAML files for various workloads can be found at **corio/config/s3/s3api/**
+YAML files for various s3 workloads can be found at **workload/s3/s3api/**
 
--- src/commons/utils/resource_util.py
+++ src/commons/utils/resource_util.py
@@ -20,6 +20,7 @@
 """Module to collect resource utilisation utils."""
 
 import os
+import shutil
 import glob
 import logging
 from src.commons.utils.support_bundle_utils import execute_remote_command
@@ -27,9 +28,10 @@ from src.commons.utils.support_bundle_utils import copy_file_from_remote
 from src.commons.utils.corio_utils import run_local_cmd
 from src.commons import constants as cm_cmd
 from src.commons.constants import MOUNT_DIR
+from src.commons.constants import ROOT
 from config import CLUSTER_CFG
 
-LOGGER = logging.getLogger("corio")
+LOGGER = logging.getLogger(ROOT)
 
 
 # pylint: disable-msg=too-many-statements
@@ -43,6 +45,10 @@ def collect_resource_utilisation(action: str):
     worker_node = []
     for node in CLUSTER_CFG["nodes"]:
         if node["node_type"] == "master":
+            if not node.get("hostname", None):
+                LOGGER.critical("failed to get master details so will not able to collect system"
+                                " stats for cluster. Nodes: '%s'", CLUSTER_CFG["nodes"])
+                continue
             host, user, passwd = node["hostname"], node["username"], node["password"]
             resp = execute_remote_command(cm_cmd.K8S_WORKER_NODES, host, user, passwd)
             LOGGER.debug("response is: %s", str(resp))
@@ -55,25 +61,33 @@ def collect_resource_utilisation(action: str):
         LOGGER.debug("Local response: %s", str(resp))
         resp = run_local_cmd(cm_cmd.CMD_RUN_NMON)
         LOGGER.debug("Local response: %s", str(resp))
+        if not host:
+            LOGGER.critical("Will not able to collect system stats for cluster as details not "
+                            "provided in cluster config.")
+            return
         resp = execute_remote_command(cm_cmd.CMD_YUM_NMON, host, user, passwd)
         LOGGER.debug("master response: %s", str(resp))
         resp = execute_remote_command(cm_cmd.CMD_RUN_NMON, host, user, passwd)
         LOGGER.debug("master response: %s", str(resp))
         for worker in worker_node:
-            host = worker
-            resp = execute_remote_command(cm_cmd.CMD_YUM_NMON, host, user, passwd)
+            resp = execute_remote_command(cm_cmd.CMD_YUM_NMON, worker, user, passwd)
             LOGGER.debug("worker response: %s", str(resp))
-            resp = execute_remote_command(cm_cmd.CMD_RUN_NMON, host, user, passwd)
+            resp = execute_remote_command(cm_cmd.CMD_RUN_NMON, worker, user, passwd)
             LOGGER.debug("worker response: %s", str(resp))
     else:
         resp = run_local_cmd(cm_cmd.CMD_KILL_NMON)
+        LOGGER.debug(resp)
         stat_fpath = sorted(glob.glob(os.getcwd() + '/*.nmon'),
                             key=os.path.getctime, reverse=True)[-1]
         LOGGER.info(stat_fpath)
         dpath = os.path.join(MOUNT_DIR, "system_stats", "client")
         if not os.path.exists(dpath):
             os.makedirs(dpath)
-        os.rename(stat_fpath, os.path.join(dpath, os.path.basename(stat_fpath)))
+        shutil.move(stat_fpath, os.path.join(dpath, os.path.basename(stat_fpath)))
+        if not host:
+            LOGGER.critical("Will not able to collect system stats for cluster as details not "
+                            "provided in cluster config.")
+            return
         resp = execute_remote_command(cm_cmd.CMD_KILL_NMON, host, user, passwd)
         LOGGER.debug("master response: %s", str(resp))
         resp = execute_remote_command(cm_cmd.CMD_NMON_FILE, host, user, passwd)
@@ -88,14 +102,13 @@ def collect_resource_utilisation(action: str):
         resp = execute_remote_command(cm_cmd.CMD_RM_NMON.format(filename), host, user, passwd)
         LOGGER.debug("file removed: %s", resp)
         for worker in worker_node:
-            host = worker
-            resp = execute_remote_command(cm_cmd.CMD_KILL_NMON, host, user, passwd)
+            resp = execute_remote_command(cm_cmd.CMD_KILL_NMON, worker, user, passwd)
             LOGGER.debug("worker response: %s", str(resp))
-            resp = execute_remote_command(cm_cmd.CMD_NMON_FILE, host, user, passwd)
+            resp = execute_remote_command(cm_cmd.CMD_NMON_FILE, worker, user, passwd)
             resp = resp[1].read(-1).decode()
             filename = str([x.strip("./") for x in resp.strip().split("\n")][0])
             LOGGER.info("Filename is: %s", filename)
             cl_path = os.path.join(client_path, filename)
-            copy_file_from_remote(host, user, passwd, cl_path, f"/root/{filename}")
-            resp = execute_remote_command(cm_cmd.CMD_RM_NMON.format(filename), host, user, passwd)
+            copy_file_from_remote(worker, user, passwd, cl_path, f"/root/{filename}")
+            resp = execute_remote_command(cm_cmd.CMD_RM_NMON.format(filename), worker, user, passwd)
             LOGGER.debug("file removed: %s", resp)
