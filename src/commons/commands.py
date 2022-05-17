50843e656e8c19516325c218f5d0611fc70b3d5d
akshaym99 <92678608+akshaym99@users.noreply.github.com>
2022-05-17 20:22:00 +0530

    nimon file added (#59)
    * nimon file added
    
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>
    
    * Logger debug added
    
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>
    
    * comment addressed
    
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>
    
    * comment addressed
    
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>
    Signed-off-by: Vikas Kumar Choudhary <vikas.kumar@seagate.com>

--- src/commons/commands.py
+++ src/commons/commands.py
@@ -43,3 +43,22 @@ CMD_HCTL_STATUS = "hctl status --json"
 CMD_GENERATE_CLSTR_LOGS = "cd {}; sh {}"
 # System commands.
 CMD_MOUNT = "mount -t nfs {} {}"
+
+# nmon installation
+CMD_YUM_NMON = "yum install -y nmon"
+K8S_WORKER_NODES = "kubectl get nodes -l node-role.kubernetes.io/worker=worker | awk '{print $1}'"
+CMD_RUN_NMON = "nmon -f -s 60 -TU"
+CMD_KILL_NMON = "kill -USR2 $(ps ax | grep nmon  | grep -v grep | awk '{print $1}')"
+CMD_NMON_FILE = "find . -name '*.nmon'"
+CMD_RM_NMON = "rm -f {}"
+# nimon installation
+CHECK_RPM = "rpm -qa | grep {}"
+CHECK_H = "{} -h"
+YUM_UNZIP = "yum install -y unzip"
+CMD_WGET_NIMON = "wget http://sourceforge.net/projects/nmon/files/njmon_linux_binaries_v78.zip" \
+                 " --no-check-certificate"
+UNZIP_NIMON = "unzip njmon_linux_binaries_v78.zip"
+CMD_CHMOD = "chmod 655 ninstall"
+CMD_NINSTALL = "./ninstall"
+CMD_RUN_NIMON = "nimon -s 30 -k -i ssc-vm-g4-rhev4-0125.colo.seagate.com -p 8086 -x njmon -W"
+CMD_KILL_NIMON = "kill -USR2 $(ps ax | grep nimon  | grep -v grep | awk '{print $1}')"
