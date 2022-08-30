f086c0b8cae48b2ea8c58eef177413c4b75c9af0
akshaym99 <92678608+akshaym99@users.noreply.github.com>
2022-08-30 12:02:36 +0530

    Copy object RVF (#111)
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>
    
    Signed-off-by: akshaym99 <akshay.s.mankar@seagate.com>

--- scripts/s3/s3api/copy_object.py
+++ scripts/s3/s3api/copy_object.py
@@ -59,37 +59,32 @@ class TestS3CopyObjects(S3Api):
         else:
             self.finish_time = datetime.now() + timedelta(hours=int(100 * 24))
 
-    @classmethod
-    def initialize_variables(cls, test_id):
-        """Initialize variables for copy object operations."""
-        cls.bucket_name1 = f"copy-obj-bucket1-{test_id}-{perf_counter_ns()}"
-        cls.bucket_name2 = f"copy-obj-bucket2-{test_id}-{perf_counter_ns()}"
-        cls.object_name1 = f"copy-object1-{test_id}-{perf_counter_ns()}"
-        cls.object_name2 = f"copy-object2-{test_id}-{perf_counter_ns()}"
-
     # pylint: disable=broad-except
     async def execute_copy_object_workload(self):
         """Execute copy object workload for specific duration."""
-        self.initialize_variables(self.test_id)
-        await self.create_bucket(self.bucket_name1)
-        self.log.info("Created bucket %s", self.bucket_name1)
-        await self.create_bucket(self.bucket_name2)
-        self.log.info("Created bucket %s", self.bucket_name2)
+        session_id = self.session_id.split("_", 1)[1]
+        bucket_name1 = f'copy-obj-bkt1-{self.test_id}-{session_id}-{perf_counter_ns()}'.lower()
+        bucket_name2 = f'copy-obj-bkt2-{self.test_id}-{session_id}-{perf_counter_ns()}'.lower()
+        object_name1 = f'copy-obt1-{self.test_id}-{session_id}-{perf_counter_ns()}'.lower()
+        object_name2 = f'copy-obt2-{self.test_id}-{session_id}-{perf_counter_ns()}'.lower()
+        await self.create_bucket(bucket_name1)
+        self.log.info("Created bucket %s", bucket_name1)
+        await self.create_bucket(bucket_name2)
+        self.log.info("Created bucket %s", bucket_name2)
         while True:
             try:
                 self.log.info("Iteration %s is started for %s...", self.iteration, self.session_id)
-                # Put object in self.bucket_name1
+                # Put object in bucket_name1
                 file_size = await self.get_workload_size()
-                file_path = corio_utils.create_file(self.object_name1, file_size)
-                self.log.info("Object1 '%s', object size %s", self.object_name1,
+                file_path = corio_utils.create_file(object_name1, file_size)
+                self.log.info("Object1 '%s', object size %s", object_name1,
                               corio_utils.convert_size(file_size))
-                await self.upload_object(self.bucket_name1, self.object_name1, file_path=file_path)
+                await self.upload_object(bucket_name1, object_name1, file_path=file_path)
                 self.log.info("Objects '%s' uploaded successfully.", self.s3_url)
-                ret1 = await self.head_object(self.bucket_name1, self.object_name1)
-                await self.copy_object(self.bucket_name1, self.object_name1, self.bucket_name2,
-                                       self.object_name2)
+                ret1 = await self.head_object(bucket_name1, object_name1)
+                await self.copy_object(bucket_name1, object_name1, bucket_name2, object_name2)
                 self.log.info("Copied object '%s in same account successfully.", self.s3_url)
-                ret2 = await self.head_object(self.bucket_name2, self.object_name2)
+                ret2 = await self.head_object(bucket_name2, object_name2)
                 assert ret1["ETag"] == ret2["ETag"], \
                     f"etag of original object ({ret1['ETag']})\netag of copied object " \
                     f"({ret2['ETag']}) are not matching"
@@ -101,23 +96,25 @@ class TestS3CopyObjects(S3Api):
                         range_read = self.range_read
                     self.log.info("Get object with range read '%s' bytes.", range_read)
                     offset = random.randrange(file_size - range_read)
-                    await self.data_integrity(byte_range=f'bytes={offset}-{range_read + offset}')
+                    await self.data_integrity(bucket_name1, bucket_name2, object_name1,
+                                              object_name2,
+                                              byte_range=f'bytes={offset}-{range_read + offset}')
                 else:
                     self.log.info("Download source and destination object and compare checksum.")
-                    await self.data_integrity()
+                    await self.data_integrity(bucket_name1, bucket_name2, object_name1, object_name2)
                 self.log.info("Delete source object from bucket-1.")
-                await self.delete_object(self.bucket_name1, self.object_name1)
+                await self.delete_object(bucket_name1, object_name1)
                 self.log.info("List destination object from bucket-2.")
-                await self.head_object(self.bucket_name2, self.object_name2)
+                await self.head_object(bucket_name2, object_name2)
                 self.log.info("Delete destination object from bucket-2.")
-                await self.delete_object(self.bucket_name2, self.object_name2)
+                await self.delete_object(bucket_name2, object_name2)
                 os.remove(file_path)
                 self.log.info("Iteration %s is completed of %s...", self.iteration, self.session_id)
             except Exception as err:
                 self.log.exception("bucket url: {%s}\nException: {%s}", self.s3_url, err)
                 assert False, f"bucket url: {self.s3_url}\nException: {err}"
             if (self.finish_time - datetime.now()).total_seconds() < MIN_DURATION:
-                for bucket in [self.bucket_name1, self.bucket_name2]:
+                for bucket in [bucket_name1, bucket_name2]:
                     self.log.info("Delete bucket %s with all objects in it.", bucket)
                     await self.delete_bucket(bucket, True)
                 return True, "Copy Object execution completed successfully."
@@ -133,17 +130,18 @@ class TestS3CopyObjects(S3Api):
             file_size = self.object_size
         return file_size
 
-    async def data_integrity(self, byte_range: str = None) -> None:
+    async def data_integrity(self, bucket_name1, bucket_name2, object_name1, object_name2,
+                             byte_range: str = None) -> None:
         """Download/read source and destination object and compare checksum."""
         if byte_range:
             self.log.info("Reading chunk %s", byte_range)
             checksum1 = await self.get_s3object_checksum(
-                bucket=self.bucket_name1, key=self.object_name1, ranges=byte_range)
+                bucket=bucket_name1, key=object_name1, ranges=byte_range)
             checksum2 = await self.get_s3object_checksum(
-                bucket=self.bucket_name2, key=self.object_name2, ranges=byte_range)
+                bucket=bucket_name2, key=object_name2, ranges=byte_range)
         else:
-            checksum1 = await self.get_s3object_checksum(self.bucket_name1, self.object_name1)
-            checksum2 = await self.get_s3object_checksum(self.bucket_name2, self.object_name2)
+            checksum1 = await self.get_s3object_checksum(bucket_name1, object_name1)
+            checksum2 = await self.get_s3object_checksum(bucket_name2, object_name2)
         assert checksum1 == checksum2, f"SHA256 ({checksum1}) of original object " \
-                                       f"({self.object_name1}) and SHA256 ({checksum2}) of copied" \
-                                       f" object ({self.object_name2}) are not matching."
+                                       f"({object_name1}) and SHA256 ({checksum2}) of copied" \
+                                       f" object ({object_name2}) are not matching."
